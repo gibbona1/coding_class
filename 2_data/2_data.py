5d4978b703a80dc09a711ebf5169783323b34f5a
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-06-27 11:02:30 +0100

    Create 2_data.py

---
+++ 2_data/2_data.py
@@ -0,0 +1,111 @@
+name = "John"
+age = 20 #integer/int
+
+salary = 123.95 #float
+
+#print(int(4.0 / 2)) #dividing and rounding down to int
+
+#print(int(salary))
+
+#print('mytable1.0.new'.split('.'))
+
+#Boolean
+is_weekday = True
+is_weekend = False
+
+#print(5 == 4)
+
+#list
+
+my_list = [1, 2, 3, "hello", "apple", True, [1, 2]]
+
+#print(my_list)
+
+my_list.append('new string')
+
+#print(my_list)
+
+extracted_element = my_list.pop(3)
+#print(extracted_element)
+#print(my_list)
+
+#more things with strings
+name = "anthony"
+#print(name.upper())
+#print(name.lower())
+#print(name.capitalize())
+
+#Control structures
+
+#if/else statments
+name = 'Anthony'
+age = 28
+if age == 18:
+    print(f"{name} can just about buy alcohol")
+elif age > 18:
+    print(f"{name} can buy alcohol")
+else:
+    print(f"{name} can't buy alcohol")
+
+#for loop
+numbers = [1, 3, 6, 11, 24]
+
+num = numbers[0]
+print(f"number is {num}")
+num = numbers[1]
+print(f"number is {num}")
+print(f"number is {numbers[2]}")
+print(f"number is {numbers[3]}")
+print(f"number is {numbers[4]}")
+print()
+
+for num in numbers:
+    print(f"number is {num}")
+    print("done with this num")
+
+print()
+
+classroom = ['john', 'mary', 'sean']
+
+for name in classroom:
+    print(f"{name} is in the class")
+    
+print()
+#num = 10
+
+for num in [10,9,8,7,6,5,4,3,2,1]:
+    print(f'{num} green bottles hanging on the wall,')
+    print(f'{num} green bottles hanging on the wall,')
+    print('And if one green bottle should accidentally fall,')
+    if num == 1:
+        print("There'll be no green bottles hanging on the wall.")
+    else:
+        print(f'There\'ll be {num-1} green bottles hanging on the wall.')
+    print()
+
+#functions
+#a set of instructions, takes in some inputs and produces an output
+
+def say_hello(name):
+    name = name.upper()
+    return f'hello, {name}'
+print(say_hello('Anthony'))
+
+for person_name in ['Anthony', 'Meg', 'Aibhe']:
+    print(say_hello(person_name))
+
+print()
+
+def make_bottle_rhyhme(num_starting_bottles):
+    for num in range(num_starting_bottles, 0, -1):
+        print(f'{num} green bottles hanging on the wall,')
+        print(f'{num} green bottles hanging on the wall,')
+        print('And if one green bottle should accidentally fall,')
+        if num == 1:
+            print("There'll be no green bottles hanging on the wall.")
+        else:
+            print(f'There\'ll be {num-1} green bottles hanging on the wall.')
+        print()
+    return 
+
+make_bottle_rhyhme(1)
