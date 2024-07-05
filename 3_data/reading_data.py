a6e0fca9bab436f153124bbd14b8d3bec4fb84fc
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-07-05 12:27:06 +0100

    add comments to scripts

--- 3_data/reading_data.py
+++ 3_data/reading_data.py
@@ -1,11 +1,17 @@
 import pandas as pd
 
+#reading from database
+##SELECT movie_table from my_database
+
+#or from an API, where you give instructions to a URL to get data, using curl/python
+##curl -G "https://api.example.com/data" -d "param1=value1" -d "param2=value2"
+
+#read from csv
 df = pd.read_csv('movie_data.csv')
 
-#SELECT movie_table from my_database
-
-#API
-#curl/python
-
+#filter to just those films released in 2024
 df_sub = df[df['Year'] == 2024]
-df_sub[df_sub['Runtime (mins)'] > 120][['Title', 'Runtime (mins)']]
+#df_sub[df_sub['Runtime (mins)'] > 120][['Title', 'Runtime (mins)']]
+
+#save filtered data to CSV (ensure it's a different name)
+df_sub.to_csv('movie_data_2024.csv')
--- 3_data/week3.py
+++ 3_data/week3.py
@@ -1,40 +1,65 @@
+#import the pandas library, which has a collection of functions for working with data
+#as shorthand, we import it as 'pd'
 import pandas as pd 
 
-# Creating the dataset
+# Creating the dataset. This is the manual setup
 data = {
     "Age": [23, 45, None, 33, 28, 40, 50, 29, 34, 48, 33, 37, 22, 46, 30, 41, 27, 38, 32, 36],
     "Salary": [50000, 80000, 60000, None, 55000, 70000, 90000, 58000, 64000, 85000, 62000, 67000, 52000, 82000, 61000, 73000, 54000, 68000, 63000, 66000],
     "Department": ["HR", "Finance", "IT", "HR", "HR", "Marketing", "Finance", "HR", "IT", "Finance", "IT", "Marketing", "HR", "Finance", "IT", "Marketing", "HR", "Marketing", "IT", "Marketing"]
 }
 
+#this converts the object above to a pandas DataFrame
 df = pd.DataFrame(data)
-#print(len(df))
-#df = df.dropna()
+
+#show whole dataset
+print(df)
+
+#show first few rows
+print(df.head())
+
+#print the number of rows
+print(len(df))
+
+#drop any rows with NAs (in python as NaN) present
+df = df.dropna()
+
+#alternatively, fill these NAs with some value
 #df = df.fillna({'Age': 0, 'Salary': 1000})
+
+#if you used dropna, this might a be different number of rows
 #print(len(df))
 
+#sort the data by Age in ascending order
 #print(df.sort_values(by='Age'))
 
+#descending order
+#print(df.sort_values(by='Age', ascending=[False]))
+
+#show one column
+print(df['Salary'])
+
+#show multiple columns (in any order)
 print(df[['Salary', 'Department']])
-#print(df)
 
-#print(len(df))
-
-#print(df['Age'])
-
+#show all columns in the data
 #print(df.columns)
 
-#print(df.head(2))
+#filtering data
+print(df[(df['Age'] >= 40) & 
+        (df['Department'] == 'Finance') &
+        (df['Salary'] < 85000)
+    ])
 
-#print(df[(df['Age'] >= 40) & 
-#        (df['Department'] == 'Finance') &
-#        (df['Salary'] < 85000)
-#    ])
-    
+#check the type of df (should be pandas.core.frame.DataFrame)
+#print(type(df))
+
+#check type of the age column (should be pandas.core.series.Series)
 #print(type(df['Age']))
+
+#get the average of the salary column
 #average_salary = df['Salary'].median()
 #print(average_salary)
 
 #average age
 #print(int(df['Age'].mean()))
-
